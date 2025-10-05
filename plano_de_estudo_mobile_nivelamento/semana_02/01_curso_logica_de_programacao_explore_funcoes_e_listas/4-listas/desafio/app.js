6817730460051e32cf13735199efb3552decf938
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-10-04 22:13:37 -0300

    Adicionando conteúdo - Lógica de programação: explore funçoes e listas

---
+++ plano_de_estudo_mobile_nivelamento/semana_02/01_curso_logica_de_programacao_explore_funcoes_e_listas/4-listas/desafio/app.js
@@ -0,0 +1,23 @@
+/*Crie uma lista vazia, com o nome listaGenerica.*/
+let listaGenerica = [];
+console.log(typeof(listaGenerica));
+
+/*Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.*/
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+/*Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.*/
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.*/
+let listaDeNomes = ['Lucas', 'Tampinha', 'Tampão'];
+console.log(listaDeNomes[0]);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.*/
+let listaDeNomes2 = ['Lucas', 'Tampinha', 'Tampão'];
+console.log(listaDeNomes2[1]);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o último elemento.*/
+let listaDeNome3 = ['Lucas', 'Tampinha', 'Tampão'];
+console.log(listaDeNome3[2]);
