b97dafeccc19c48fd9b12dccaa6ddc8eb47ab966
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-11-12 11:49:51 -0300

    termino aula - refazendo uma operação

---
+++ plano_de_estudo_mobile_nivelamento/semana_05/01_dart_trabalhando_com_a_sintaxe_e_configuracao_de_projeto/04_aulas_listas_recursao_e_mais_melhorias/03_refazendo_uma_operacao/calculadora/bin/calculadora.dart
@@ -0,0 +1,95 @@
+import 'dart:io';
+
+List<String> operacoes = <String>["+", "-", "*", "/"];
+
+void calcular(String operacao, double n1, double n2) {
+  switch (operacao) {
+    case "+":
+      adicao(n1, n2);
+      break;
+    case "-":
+      subtracao(n1, n2);
+      break;
+    case "*":
+      multiplicacao(n1, n2);
+      break;
+    case "/":
+      divisao(n1, n2);
+      break;
+    default:
+      print("Operação '$operacao' não é válida. Revise a operação digitada!");
+  }
+}
+
+String capturarOperacaoUsuario() {
+  print("Qual operação matemática deseja efetuar?");
+  print("Escolha uma entre: +, -, *, /");
+  String? entrada = stdin.readLineSync();
+
+  if ((entrada == null) || entrada == "") {
+    print("Entrada não pode ser nula!");
+    exit(0);
+  }
+
+  if (!operacoes.contains(entrada)) {
+    print("Operação $entrada inválida, escolha entre ${operacoes.toString()}");
+    return capturarOperacaoUsuario();
+  }
+  return entrada;
+}
+
+double capturarSegundoNumero() {
+  print("Digite o segundo número: ");
+  String? entrada = stdin.readLineSync();
+
+  double? numero = double.tryParse(entrada ?? "");
+
+  if (numero == null || numero.isNaN) {
+    print("Entrada $entrada não é um número válido!");
+    return capturarSegundoNumero();
+  }
+
+  return numero;
+}
+
+double capturarPrimeiroNumero() {
+  print("Digite o primeiro número: ");
+  String? entrada = stdin.readLineSync();
+
+  double? numero = double.tryParse(entrada ?? "");
+
+  if (numero == null || numero.isNaN) {
+    print("Entrada $entrada não é um número válido!");
+    return capturarPrimeiroNumero();
+  }
+  return numero;
+}
+
+void adicao(double n1, double n2) {
+  double resultado = n1 + n2;
+  print("$n1 + $n2 = $resultado");
+}
+
+void subtracao(double n1, double n2) {
+  double resultado = n1 - n2;
+  print("$n1 - $n2 = $resultado");
+}
+
+void multiplicacao(double n1, double n2) {
+  double resultado = n1 * n2;
+  print("$n1 * $n2 = $resultado");
+}
+
+void divisao(double n1, double n2) {
+  double resultado = n1 / n2;
+  print("$n1 / $n2 = $resultado");
+}
+
+void main() {
+  print("### CALCULADORA ###");
+  var operacao = capturarOperacaoUsuario();
+  var n1 = capturarPrimeiroNumero();
+  var n2 = capturarSegundoNumero();
+
+  calcular(operacao, n1, n2);
+}
