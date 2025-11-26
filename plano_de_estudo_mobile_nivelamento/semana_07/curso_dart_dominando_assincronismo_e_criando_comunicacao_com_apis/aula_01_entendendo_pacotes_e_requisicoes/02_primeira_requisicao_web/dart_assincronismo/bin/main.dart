cab88aec1e2960c4ea1d1a9121b75880b1b9cbf6
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-11-26 17:47:14 -0300

    primeira requisição para web com dart

--- plano_de_estudo_mobile_nivelamento/semana_07/curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/aula_01_entendendo_pacotes_e_requisicoes/02_primeira_requisicao_web/dart_assincronismo/bin/main.dart
+++ plano_de_estudo_mobile_nivelamento/semana_07/curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/aula_01_entendendo_pacotes_e_requisicoes/02_primeira_requisicao_web/dart_assincronismo/bin/main.dart
@@ -1,3 +1,11 @@
+import 'package:http/http.dart';
+
 void main() {
-  print('Olá mundo');
+  requestData();
+}
+
+requestData() {
+  String url =
+      'https://gist.githubusercontent.com/Lucasgyn94/8105fe8b423547e2cd1368ce8d9d9319/raw/ea32a8b50b84fdf7f40ac38b64ba8a7a2a6e34f6/accounts.json';
+  print(get(Uri.parse(url)));
 }
