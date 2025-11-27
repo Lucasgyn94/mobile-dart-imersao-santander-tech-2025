c3935ce22b543b1c46c02979396b37c3df9fbc0c
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-11-27 15:26:50 -0300

    modificando tipo retorno do metodo

--- plano_de_estudo_mobile_nivelamento/semana_07/curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/aula_01_entendendo_pacotes_e_requisicoes/05_lista_de_exercicios/dart_receitas_app/bin/main.dart
+++ plano_de_estudo_mobile_nivelamento/semana_07/curso_dart_dominando_assincronismo_e_criando_comunicacao_com_apis/aula_01_entendendo_pacotes_e_requisicoes/05_lista_de_exercicios/dart_receitas_app/bin/main.dart
@@ -4,10 +4,10 @@ void main() {
   fetchRecipes();
 }
 
-fetchRecipes() {
+void fetchRecipes() {
   String url =
       'https://gist.githubusercontent.com/Lucasgyn94/065a679bb2e77a2206de43cdfaea59d9/raw/c4a672836b9d93399888662eb631c857d43bf45f/recipes.json';
-  var futureResponse = get(Uri.parse(url));
+  Future<Response> futureResponse = get(Uri.parse(url));
 
   futureResponse.then((Response response) {
     print(response.body);
